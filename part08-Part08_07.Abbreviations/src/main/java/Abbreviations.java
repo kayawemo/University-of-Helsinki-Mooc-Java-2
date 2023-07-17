6747159ad84e42bf0c194376fbf6d90c9cfd1f58
Kayode-Awe <kayode.awe@outlook.com>
2023-07-17 08:34:34 -0600

    completed part8-2

---
+++ part08-Part08_07.Abbreviations/src/main/java/Abbreviations.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+public class Abbreviations {
+
+    private HashMap<String, String> abbreviations;
+
+    public Abbreviations() {
+        abbreviations = new HashMap<>();
+    }
+
+    public void addAbbreviation (String abbreviation, String explanation) {
+        this.abbreviations.put(abbreviation, explanation);
+    }
+
+    public boolean hasAbbreviation (String abbreviation) {
+        if (abbreviations.containsKey(abbreviation)) {
+            return true;
+        }
+        return false;
+    }
+
+    public String findExplanationFor (String abbreviation) {
+        if(hasAbbreviation(abbreviation)) {
+            return abbreviations.get(abbreviation);
+        }
+        return  null;
+    }
+}
